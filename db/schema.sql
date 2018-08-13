d74b41dffbdd12ba7e4c856d43b3950166dd9cab
wood0761 <wood0761@gmail.com>
2018-08-13 18:30:14 -0500

    commit

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers 
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(20) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California burger', false);
